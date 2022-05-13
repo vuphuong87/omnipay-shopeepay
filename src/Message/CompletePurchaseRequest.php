74aa81ead9ee72019457738942ae3709554aabd1
Khanh Nguyen <quockhahnguyen1997@gmail.com>
2022-05-13 21:59:34 +0700

    Refactoring complete(),

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Omnipay\Shopeepay\Message;
 
 
+use function GuzzleHttp\json_encode;
 class CompletePurchaseRequest extends AbstractRequest
 {
     public function getData(): array
@@ -12,23 +13,23 @@ class CompletePurchaseRequest extends AbstractRequest
         $request = $this->httpRequest->request->all();
 
         $order = [
-            'amount'               => $request['amount'] ? (int)$request['amount'] / 100 : null,
-            'merchant_ext_id'      => $request['merchant_ext_id'] ?? null,
-            'payment_reference_id' => $request['payment_reference_id'] ?? null,
+            'amount'               => $request['amount'] ?? null,
             'payment_method'       => $request['payment_method'] ?? null,
-            'payment_status'       => $request['payment_status'] ?? null,
-            'reference_id'         => $request['reference_id'] ?? null,
+            'merchant_ext_id'      => $request['merchant_ext_id'] ?? null,
             'store_ext_id'         => $request['store_ext_id'] ?? null,
-            'terminal_id'          => $request['terminal_id'] ?? null,
             'transaction_sn'       => $request['transaction_sn'] ?? null,
+            'transaction_type'     => $request['transaction_type'] ?? null,
             'transaction_status'   => $request['transaction_status'] ?? null,
+            'reference_id'         => $request['reference_id'] ?? null,
             'user_id_hash'         => $request['user_id_hash'] ?? null,
-            'state'                => $request['transaction_status'],
+            'payment_reference_id' => $request['payment_reference_id'] ?? null,
+            'payment_status'       => $request['payment_status'] ?? null,
+            'terminal_id'          => $request['terminal_id'] ?? null,
         ];
 
-        $order['computed_signature'] = $this->computeSignature(
-            implode('', array_values($order))
-        );
+        $order['computed_signature'] = $this->computeSignature(json_encode(array_filter($order)));
+        $order['signature'] = $this->httpRequest->headers->get('x-airpay-req-h');
+        $order['state'] = $request['transaction_status'];
 
         return $order;
     }
